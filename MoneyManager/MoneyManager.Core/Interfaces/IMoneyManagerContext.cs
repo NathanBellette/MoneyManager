a687bb4ea7bce73b4d5baf4bd0bab7fceb83e17c
Nathan Bellette <nath78@gmail.com>
2015-05-25 14:28:40 +1000

    Added IMoneyManager interface

---
+++ MoneyManager/MoneyManager.Core/Interfaces/IMoneyManagerContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using MoneyManager.Core.Models;
+
+namespace MoneyManager.Core.Interfaces
+{
+    public interface IMoneyManagerContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Category> Categories { get; set; } 
+        DbSet<T> Set<T>() where T : class;
+        DbEntityEntry Entry(object entry);
+        int SaveChanges();
+        void Dispose();
+    }
+}
